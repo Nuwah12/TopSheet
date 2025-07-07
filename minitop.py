04b225bb8ca5c6416f7330ee5fd84f3029b1de13
Nuwah12 <ngb23@pitt.edu>
2025-07-07 15:47:34 -0400

    proof of concept

---
+++ minitop.py
@@ -0,0 +1,44 @@
+import psutil
+import socket
+import time
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Google Sheets setup
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+client = gspread.oauth()
+sheet = client.create("Bioinfo Job Tracker").sheet1
+
+# Clean existing data
+sheet.clear()
+sheet.append_row(['Hostname', 'PID', 'Command', '%CPU', '%MEM', 'Elapsed Time', 'Last Updated'])
+
+hostname = socket.gethostname()
+
+while True:
+    now = time.strftime('%Y-%m-%d %H:%M:%S')
+    rows = []
+
+    for proc in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_percent', 'create_time', 'cmdline']):
+        try:
+            cmd = ' '.join(proc.info['cmdline']) if proc.info['cmdline'] else proc.info['name']
+            if 'minimap2' in cmd or 'flye' in cmd or 'hic' in cmd:  # customize this
+                elapsed = int(time.time() - proc.info['create_time'])
+                rows.append([
+                    hostname,
+                    proc.info['pid'],
+                    cmd[:50],
+                    round(proc.info['cpu_percent'], 2),
+                    round(proc.info['memory_percent'], 2),
+                    f"{elapsed//3600}h {(elapsed%3600)//60}m",
+                    now
+                ])
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue
+
+    # Refresh sheet
+    sheet.resize(rows=len(rows)+1)
+    sheet.update('A2', rows)
+
+    time.sleep(60)  # check every minute
+
